fb78d7cf428f9095896a10df470a1b615aa064b4
Groxworld1 <40923254+Groxworld1@users.noreply.github.com>
2019-01-10 22:17:48 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -65,7 +65,7 @@ router.get('/auth/facebook/done', passport.authenticate('facebook', { failureRed
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index');
+  res.render('index', {title: "Express"});
 });
 
 router.get('/login', function (req, res, next) {
